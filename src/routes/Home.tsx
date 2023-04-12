dab2574fcb8858b76f0946f39795392c74e75a4c
DaviJat <ddavijatoba33@gmail.com>
2023-04-12 15:05:05 -0300

    Associating API values ​​with the type User

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -11,16 +11,25 @@ const Home = () => {
     const loadUser = async (userName: string) => {
         const res = await fetch(`https://api.github.com/users/${userName}`)
 
-        console.log(res);
-
         const data = await res.json();
 
-        console.log(data);
+        const {avatar_url, login, location, followers, following } = data;
+
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following
+        }
+
+        setUser(userData);
     }
 
     return (
         <div>
             <Search loadUser={loadUser} />
+            {user && <p>{user.login}</p>}
         </div>
     )
 };
